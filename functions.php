56c4c24751e06d536ab69c17b85fd3cd01fa3135
Vincent van Haaff <vincent@vanhaaff.com>
2012-03-19 15:55:43 -0700

    now with moar awesome!

--- functions.php
+++ functions.php
@@ -6,16 +6,23 @@
 
 automatic_feed_links();
 
-if ( function_exists('register_sidebar') ) {
+function my_register_sidebars() {
 	register_sidebar(array(
-        //'name'          => sprintf(__('Sidebar of Aweeeessoommee!!!'), $i ),
-        'name'          => 'Best Sidebar Ever',
-        'id'            => 'sidebar$i',
-        'description'   => '',
+		'name'          =>  __( 'Main Sidebar' ),
+		'id'            => 'sidebar_1',
+		'description'   => '',
 		'before_widget' => '<li id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</li>',
-		'before_title'  => '<h4 class="sidebarHeader">',
-		'after_title'   => '</h4>',
+		'after_widget'  => '</li><br />',
+		'before_title'  => '<!--<h4 class="sidebarHeader">',
+		'after_title'   => '</h4>-->',
 	));
 }
+add_action( 'widgets_init', 'my_register_sidebars' );
 
+
+function register_my_menus() {
+  register_nav_menus(
+    array('header-menu' => __( 'Header Menu' ) )
+  );
+}
+add_action( 'init', 'register_my_menus' );
--- sidebar.php
+++ sidebar.php
@@ -6,33 +6,6 @@
 ?>
 	<div id="sidebar" role="complementary">		
 		<ul>
-                
-            <!-- <div id="countdown"></div> -->
-            <div id="ticketsButton"><a href="http://vancouvermakerfaire.eventbrite.com?ref=ebtn">Buy Tickets</a></div>
-            <br />
-            <div id="volunteerButton"><a href="https://vancouvermakerfaire.wufoo.com/forms/i-want-to-volunteer-in-2012/">Become a Volunteer for Vancouver Maker Faire</a></div>
-            <br />
-            <div id="mailinglistButton"><a href="http://vancouvermakerfaire.wufoo.com/forms/mailing-list-sign-up/">Join Mailing List</a></div>
-			
-			
-			<div id="sidebarPaymentSection">
-				<!-- <h4 class="sidebarHeader">Support MF Vancouver:</h4> -->
-				
-				<div id="paypalButton">
-                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                    <input type="hidden" name="cmd" value="_s-xclick">
-                    <input type="hidden" name="hosted_button_id" value="YCC57L4X89XV4">
-                    <input type="image" 
-                        src="<?php bloginfo('template_directory'); ?>/images/btn_donate_top.png" 
-                        onMouseOver="this.src='<?php bloginfo('template_directory'); ?>/images/btn_donate_bottom.png'" 
-                        onMouseOut="this.src='<?php bloginfo('template_directory'); ?>/images/btn_donate_top.png'" border="0" name="submit" alt="Donate to your local community using PayPal!">
-                    <img alt="" border="0" src="https://www.paypalobjects.com/WEBSCR-640-20110306-1/en_US/i/scr/pixel.gif" width="1" height="1">
-                    </form>
-				</div>
-			</div>
-<br />
-<br />
-
             <!--<div id="sidebarSponsorsSection">
                 <h4 class="sidebarHeader">Sponsors:</h4>
                 
@@ -57,15 +30,23 @@
 			
 		<div id="primary" class="widget-area" role="complementary">
 
-                <?php
-                /* When we call the dynamic_sidebar() function, it'll spit out
-                 * the widgets for that widget area. If it instead returns false,
-                 * then the sidebar simply doesn't exist, so we'll hard-code in
-                 * some default sidebar stuff just in case.
-                 */
-                if ( !function_exists("dynamic_sidebar") || !dynamic_sidebar("Best Sidebar Ever") ) : ?>
+		<?php
+		/* When we call the dynamic_sidebar() function, it'll spit out
+		 * the widgets for that widget area. If it instead returns false,
+		 * then the sidebar simply doesn't exist, so we'll hard-code in
+		 * some default sidebar stuff just in case.
+		 */
+		if ( !function_exists("dynamic_sidebar") || !dynamic_sidebar("sidebar_1") ) : ?>
+
+<div id="volunteerButton"><a href="https://vancouvermakerfaire.wufoo.com/forms/i-want-to-volunteer-in-2012/">Become a Volunteer for Vancouver Maker Faire</a></div>
+<br />
+
+</div>
+
+
 	
-                <?php endif; // end primary widget area ?>
+		
+		<?php endif; // end primary widget area ?>
 		</div><!-- #primary .widget-area -->
     </ul> 
 
